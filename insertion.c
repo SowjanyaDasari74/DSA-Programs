03409e40b0bfc694939a3ec1fde1aabeabb4abe8
21wh1a1273 <113002271+21wh1a1273@users.noreply.github.com>
2023-01-02 13:43:49 +0530

    Add files via upload

---
+++ insertion.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int binary(int = a[],int low,int high,int key);
+{
+int mid, i;
+mid = (low+high)/2
+for(i=0;i<mid;i++)
+{
+printf("element is found");
+}
+else
+{
+printf("element is not found");
+}
+}
+void main
+{
+int high,low,i,j;
+{
+printf("enter the key");
+scanf("%d",&key);
+}
+for(i=0;i<key;i++)
+{
+printf("element found");
+}
+for(j=0;j<i;j--);
+{
+printf("element  is not found");
+}
+}
+
+
+

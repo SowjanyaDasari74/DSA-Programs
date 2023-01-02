716542687c5a866f84c0ecd001500e63e69e3108
21wh1a1273 <113002271+21wh1a1273@users.noreply.github.com>
2023-01-02 14:45:09 +0530

    Update insertion.c

--- insertion.c
+++ insertion.c
@@ -1,33 +1,49 @@
-#include<stdio.h>
-int binary(int = a[],int low,int high,int key);
-{
-int mid, i;
-mid = (low+high)/2
-for(i=0;i<mid;i++)
-{
-printf("element is found");
-}
-else
-{
-printf("element is not found");
-}
-}
-void main
-{
-int high,low,i,j;
-{
-printf("enter the key");
-scanf("%d",&key);
-}
-for(i=0;i<key;i++)
-{
-printf("element found");
-}
-for(j=0;j<i;j--);
-{
-printf("element  is not found");
-}
-}
+#include <math.h>
+#include <stdio.h>
 
+void insertionSort(int arr[], int n)
+{
+    int i, key, j;
+    for (i = 1; i < n; i++) {
+        key = arr[i];
+        j = i - 1;
+ 
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j = j - 1;
+        }
+        arr[j + 1] = key;
+    }
+}
+ 
+void printArray(int arr[], int n)
+{
+    int i;
+    for (i = 0; i < n; i++)
+        printf("%d ", arr[i]);
+    printf("\n");
+}
+ 
+int main()
+{
+    int arr[] = { 12, 11, 13, 5, 6 };
+    int n = sizeof(arr) / sizeof(arr[0]);
+ 
+    insertionSort(arr, n);
+    printArray(arr, n);
+ 
+    return 0;
+}
+output:
+Enter number of elements
+5
+Enter 5 integers
+3 7 2 9 5
+Sorted list in ascending order:
+2
+3
+5
+7
+9
 
 

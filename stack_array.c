51df054164b8c40cae30ebabaf5c2099e4f5ec9c
21wh1a1273 <113002271+21wh1a1273@users.noreply.github.com>
2023-01-09 14:46:23 +0530

    Update stack_array.c

--- stack_array.c
+++ stack_array.c
@@ -86,3 +86,42 @@ else
 printf("\n The STACK is empty");
 }  
 }
+output:
+Enter the size of STACK[MAX=100]:10
+
+         STACK OPERATIONS USING ARRAY
+        --------------------------------
+         1.PUSH
+         2.POP
+         3.DISPLAY
+         4.EXIT
+ Enter the Choice:1
+ Enter a value to be pushed:12
+
+ Enter the Choice:1
+ Enter a value to be pushed:24
+
+ Enter the Choice:1
+ Enter a value to be pushed:98
+
+ Enter the Choice:3
+
+ The elements in STACK
+
+98
+24
+12
+ Press Next Choice
+ Enter the Choice:2
+
+         The popped elements is 98
+ Enter the Choice:3
+
+ The elements in STACK
+
+24
+12
+ Press Next Choice
+ Enter the Choice:4
+
+         EXIT POINT
